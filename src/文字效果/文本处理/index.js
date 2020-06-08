0db7d970fb2d6cb9328ec8e0d6ca8b2f3f2d14ef
ma_yawen <ma_yawen@uxsino.com>
2020-06-08 21:32:12 +0800

    feat: Demo from code-notebook. --Myw

---
+++ src/文字效果/文本处理/index.js
@@ -0,0 +1,29 @@
+import "./index.css";
+
+// * 多行文本溢出 JS 省略号。
+const text =
+  "这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本这是一段长文本";
+const totalTextLen = text.length;
+const formatStr = () => {
+  const ele = document.querySelector(".multi-line--text_wrapper__JS"),
+    lineNum = 2,
+    baseWidth = window.getComputedStyle(ele).width,
+    baseFontSize = window.getComputedStyle(ele).fontSize,
+    lineWidth = +baseWidth.slice(0, -2);
+  // 所计算的 strNum 为元素内部一行可容纳的字数（只考虑了中文）。
+  const strNum = Math.floor(lineWidth / +baseFontSize.slice(0, -2));
+  let content = "";
+  // 多行可容纳总字数
+  const totalStrNum = Math.floor(strNum * lineNum);
+  const lastIndex = totalStrNum - totalTextLen;
+  if (totalTextLen > totalStrNum) {
+    content = text.slice(0, lastIndex - 3).concat("...");
+  } else {
+    content = text;
+  }
+  ele.innerHTML = content;
+};
+formatStr();
+window.onresize = () => {
+  formatStr();
+};
