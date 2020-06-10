f1a108ec51662cc8c1a12664842a8e05ecb11eff
_MAYAWEN <mayawen@cdhengsu.com>
2020-06-11 00:45:20 +0800

    feat: Background animation add. --Myw

---
+++ src/动画/canvas-背景动画/index.js
@@ -0,0 +1,139 @@
+import "./index.css";
+
+function normalPool(o) {
+  var r = 0;
+  do {
+    var a = Math.round(normal({ mean: o.mean, dev: o.dev }));
+    if (a < o.pool.length && a >= 0) return o.pool[a];
+    r++;
+  } while (r < 100);
+}
+function randomNormal(o) {
+  if (
+    ((o = Object.assign({ mean: 0, dev: 1, pool: [] }, o)),
+    Array.isArray(o.pool) && o.pool.length > 0)
+  )
+    return normalPool(o);
+  var r,
+    a,
+    n,
+    e,
+    l = o.mean,
+    t = o.dev;
+  do {
+    r = (a = 2 * Math.random() - 1) * a + (n = 2 * Math.random() - 1) * n;
+  } while (r >= 1);
+  return (e = a * Math.sqrt((-2 * Math.log(r)) / r)), t * e + l;
+}
+/**
+ * @param { Number } NUM_PARTICLES 气泡个数；
+ * @param { Number } PARTICLE_SIZE 视口倍数；
+ * @param { Number } SPEED 动画时间（毫秒）；
+ * @param { Array } particles 存放气泡的数组。
+ */
+const NUM_PARTICLES = 600;
+const PARTICLE_SIZE = 0.5;
+const SPEED = 20000;
+let particles = [];
+
+function rand(low, high) {
+  return Math.random() * (high - low) + low;
+}
+// * canvas 中添加气泡。
+function createParticle(canvas) {
+  const colour = {
+    r: 255,
+    g: randomNormal({ mean: 125, dev: 20 }),
+    b: 50,
+    a: rand(0, 1),
+  };
+  return {
+    x: -2,
+    y: -2,
+    diameter: Math.max(
+      0,
+      randomNormal({ mean: PARTICLE_SIZE, dev: PARTICLE_SIZE / 2 })
+    ),
+    duration: randomNormal({ mean: SPEED, dev: SPEED * 0.1 }),
+    amplitude: randomNormal({ mean: 16, dev: 2 }),
+    offsetY: randomNormal({ mean: 0, dev: 10 }),
+    arc: Math.PI * 2,
+    startTime: performance.now() - rand(0, SPEED),
+    colour: `rgba(${colour.r}, ${colour.g}, ${colour.b}, ${colour.a})`,
+  };
+}
+// * 移动气泡。
+function moveParticle(particle, canvas, time) {
+  const progress =
+    ((time - particle.startTime) % particle.duration) / particle.duration;
+  return {
+    ...particle,
+    x: progress,
+    y:
+      Math.sin(progress * particle.arc) * particle.amplitude + particle.offsetY,
+  };
+}
+// * 绘制气泡。
+function drawParticle(particle, canvas, ctx) {
+  canvas = document.getElementById("particle-canvas");
+  const vh = canvas.height / 10;
+  ctx.fillStyle = particle.colour;
+  ctx.beginPath();
+  ctx.ellipse(
+    particle.x * canvas.width,
+    particle.y * vh + canvas.height / 5,
+    particle.diameter * vh,
+    particle.diameter * vh,
+    0,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
+}
+
+function draw(time, canvas, ctx) {
+  // 移动气泡。
+  particles.forEach((particle, index) => {
+    particles[index] = moveParticle(particle, canvas, time);
+  });
+  // 清除 canvas。
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // 在 canvas 中添加气泡。
+  particles.forEach((particle) => {
+    drawParticle(particle, canvas, ctx);
+  });
+  // 计算气泡的下一帧。
+  requestAnimationFrame((time) => draw(time, canvas, ctx));
+}
+// * 初始化 canvas。
+function initializeCanvas() {
+  let canvas = document.getElementById("particle-canvas");
+  canvas.width = canvas.offsetWidth * window.devicePixelRatio;
+  canvas.height = canvas.offsetHeight * window.devicePixelRatio;
+  let ctx = canvas.getContext("2d");
+  window.addEventListener("resize", () => {
+    canvas.width = canvas.offsetWidth * window.devicePixelRatio;
+    canvas.height = canvas.offsetHeight * window.devicePixelRatio;
+    ctx = canvas.getContext("2d");
+  });
+  return [canvas, ctx];
+}
+// * 开始动画函数。
+function startAnimation() {
+  const [canvas, ctx] = initializeCanvas();
+  // 创建气泡。
+  for (let i = 0; i < NUM_PARTICLES; i++) {
+    particles.push(createParticle(canvas));
+  }
+  requestAnimationFrame((time) => draw(time, canvas, ctx));
+}
+// * 页面加载完成绘制背景。
+(function () {
+  if (document.readystate !== "loading") {
+    startAnimation();
+  } else {
+    document.addEventListener("DOMContentLoaded", () => {
+      startAnimation();
+    });
+  }
+})();
