03ad9feb2a01a5bc87317acc45d6091849d7db8c
_MAYAWEN <mayawen@cdhengsu.com>
2020-06-17 01:11:32 +0800

    feat: New demo twitter-点赞按钮，not build. --Myw

---
+++ src/按钮/twitter-点赞按钮/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+
+const heartButton = document.querySelector('.heart-animation');
+heartButton.addEventListener('click', function() {
+  let classList = heartButton.classList;
+  classList.contains('like-active') ? classList.remove('like-active') : classList.add('like-active');
+});
