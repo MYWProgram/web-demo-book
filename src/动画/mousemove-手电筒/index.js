37766c69f485c71a78ab17b03c873e4520726ef0
_MAYAWEN <mayawen@cdhengsu.com>
2020-09-06 00:45:06 +0800

    fix: Delete some demos, and download assets to local. --Myw

--- src/动画/mousemove-手电筒/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-let boxElement = document.querySelector('.box');
-boxElement.onmousemove = e => {
-  let circleX = e.pageX - boxElement.offsetLeft;
-  let circleY = e.pageY - boxElement.offsetTop;
-  boxElement.style.setProperty('--x', `${circleX}px`);
-  boxElement.style.setProperty('--y', `${circleY}px`);
-};
--- src/布局/Grid-瀑布流布局/index.js
+++ src/布局/Grid-瀑布流布局/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 
 let imgs = document.querySelectorAll('img');
-Array.from(imgs).forEach((img, index) => {
+Array.from(imgs).forEach(img => {
   let src = img.getAttribute('src');
   let image = new Image();
   image.src = src;
@@ -11,7 +11,6 @@ Array.from(imgs).forEach((img, index) => {
       h = image.height;
     let height = Math.round((h * width) / w);
     img.src = src;
-    img.parentNode.parentNode.style = `width: ${width}px;`;
     img.parentNode.style = `grid-row-end: span ${height / 10};`;
   };
 });
